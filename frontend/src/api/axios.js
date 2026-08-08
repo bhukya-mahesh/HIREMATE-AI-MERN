e8142da8b332f12beeceb127266b8cf2ff827db4
bhukya-mahesh <bhukyamahesh4546@gmail.com>
2026-08-08 07:24:56 +0530

    axios confired

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,21 +1,6 @@
-import axios from "axios";
-
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = token;
-  return config;
-});
-
-export default api;
-
-
 // import axios from "axios";
 
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL || "https://hiremate-ai-mern-4buf.onrender.com/"
-// });
+// const api = axios.create({ baseURL: "http://localhost:5000/api" });
 
 // api.interceptors.request.use((config) => {
 //   const token = localStorage.getItem("token");
@@ -24,3 +9,18 @@ export default api;
 // });
 
 // export default api;
+
+
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "https://hiremate-ai-mern-4buf.onrender.com/"
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = token;
+  return config;
+});
+
+export default api;
